53f480b87f11108944269706e3b0a33938a4947f
Katlo5 <mphomookiemang@gmail.com>
2026-02-21 18:20:48 +0200

    first commit

---
+++ src/main/java/com/comps/companion/CompanionApplication.java
@@ -0,0 +1,13 @@
+package com.comps.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionApplication.class, args);
+	}
+
+}
